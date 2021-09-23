1b203bcb248998748de210bda899580cac394c1a
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-09-23 22:25:17 +0530

    added tadhack req structures

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import requests
 import os
 import json
 import time
@@ -7,20 +7,73 @@ import time
 
 app = Flask('app')
 
-@app.route('/verify', methods=['GET', 'POST'])
-def test():
-    if request.method == 'GET':
-        return "<h1>verify path works</h1>"
-    if request.method == 'POST':
-        return "POST hit"
+APP_ID = "APP_000375"
+PASSWORD = "a07118cda5215fc6d01db5b2ab848edd"
+DEFAULT_SUBSCRIBER_ID= "tel:94776177301"
+APP_HASH = "abcdefgh"
 
-@app.route('/generate, methods=['GET', 'POST'])
-def generate):
+current_ref_num = "213561321321613"
+current_otp = "123564"
+verify_url = "https://api.hSenidMobile.lk/otp/verify"
+generate_url = "https://api.hSenidMobile.lk/otp/request"
+
+generate_params = {
+    "applicationId": APP_ID,
+    "password": PASSWORD,
+    "subscriberId": DEFAULT_SUBSCRIBER_ID,
+    "applicationHash": APP_HASH,
+}
+
+verify_params = {
+    "applicationId": APP_ID,
+    "password": PASSWORD,
+    "referenceNo": current_ref_num,
+    "otp": current_otp
+}
+
+@app.route('/generate', methods=['GET', 'POST'])
+def generate():
     if request.method == 'GET':
-        return "<h1>generate path works</h1>"
-    if request.method == 'POST':
+        return "<h1>Generate path works</h1>"
+    elif request.method == 'POST':
+        try:
+            tel_str = request.args.get("tel")
+            gen_params = generate_params
+            gen_params["subscriberId"] = tel_str
+            response = requests.post(generate_url, params=gen_params)
+            response_content  = dict(response.content)
+            current_ref_num = response_content["referenceNo"]
+            print(current_ref_num)
+            return "success"
+        except Exception as e:
+            print(e)
+            return "Something Went Wrong"
         return "POST hit"
+    else:
+        return "Unknown Request type."
+
+@app.route('/verify', methods=['GET', 'POST'])
+def verify():
+    if request.method == 'GET':
+        return "<h1>Verify path works</h1>"
+    elif request.method == 'POST':
+        try:
+            otp = request.args.get("otp")
+            verify_params = verify_params
+            verify_params["otp"] = otp
+            response = requests.post(generate_url, params=verify_params)
+            response_content  = dict(response.content)
+            status = response_content["subscriptionStatus"]
+            # return "POST hit"
+            print(status)
+            return status
+        except Exception as e:
+            print(e)
+            return "Something Went Wrong"
+    else:
+        "Unknown Request type."
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0')
+    app.run()
