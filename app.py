4da31fcc673e2b43e8b2a9a9b2b3e33856399a0d
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-09-24 19:14:52 +0530

    update

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def verify():
                 raise Exception("OTP not provided")
             verify_p = verify_params
             verify_p["otp"] = otp
-            print(otp)
+            print("otp is", otp)
             return json.dumps({"code":200, "status": "valid"})
             # verify_p["referenceNo"] = current_ref_num
             response = requests.post(verify_url, headers=headers_dict, data=json.dumps(verify_p))
